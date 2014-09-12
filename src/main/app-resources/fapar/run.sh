c8f87a1199de26ed49b1fe86ef3c4bf1a8304758
fdandria <fdandria@vm-10-10-31-52.dev.terradue.int>
2014-09-12 18:01:10 +0200

    [fapar] first working version

--- src/main/app-resources/fapar/run.sh
+++ src/main/app-resources/fapar/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Project:       ${project.name}
-# Author:        $Author: fbrito $ (Terradue Srl)
-# Last update:   ${doc.timestamp}:
-# Element:       ${project.name}
-# Context:       ${project.artifactId}
-# Version:       ${project.version} (${implementation.build})
-# Description:   ${project.description}
+# Project:		 ${project.name}
+# Author:		  $Author: fbrito $ (Terradue Srl)
+# Last update:	${doc.timestamp}:
+# Element:		 ${project.name}
+# Context:		 ${project.artifactId}
+# Version:		 ${project.version} (${implementation.build})
+# Description:	${project.description}
 #
 # This document is the property of Terradue and contains information directly
 # resulting from knowledge and experience of Terradue.
@@ -28,26 +28,59 @@ ERR_NOINPUT=1
 # add a trap to exit gracefully
 function cleanExit ()
 {
-   local retval=$?
-   local msg=""
-   case "$retval" in
-     $SUCCESS)          msg="Processing successfully concluded";;
-     $ERR_NOINPUT) msg="Input not retrieved to local node";;
-     *)                 msg="Unknown error";;
-   esac
-   [ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
-   exit $retval
+	local retval=$?
+	local msg=""
+	case "$retval" in
+		$SUCCESS) 	msg="Processing successfully concluded";;
+		$ERR_NOINPUT)	msg="Input not retrieved to local node";;
+		*)		msg="Unknown error";;
+	esac
+
+	[ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
+
+	exit $retval
 }
+
 trap cleanExit EXIT
 
-inputsdir=${TMPDIR}/inputs
+ciop-log "DEBUG" "Input dir: ${TMPDIR}/input"
+ciop-log "DEBUG" "Ouput dir: ${TMPDIR}/output"
+
+inputDir=${TMPDIR}/input
+outputDir=${TMPDIR}/output
+
+mkdir -p $inputDir
+mkdir -p $outputDir
+
+export DISPLAY=:99
+ciop-log "DEBUG" "Running on display $DISPLAY"
+
 
 while read input
 do
-	mkdir -p $inputsDir
-	file=`echo $input | ciop-copy -o $inputsdir`
+	ciop-log "INFO" "Working with file $input"
+	file=`ciop-copy -o $inputDir -U $input`
+
+	ciop-log "DEBUG" "ciop-copy output is $file"
+
+	file=`basename $file`
+
 	#call the fapar su $file
-	
-	#then
-	rm -rf ${TMPDIR}
+	cd $inputDir; unzip $file; rm -f $file; cd `echo $file | sed 's#\.zip##g'`
+
+	ciop-log "DEBUG" "File is $file"
+
+	myInputDir="$inputDir/`echo $file | sed 's#\.zip##g'`/"
+	myOutputDir="$outputDir/`echo $file | sed 's#\.zip##g' | sed 's#input#output#g'`/"; mkdir -p $myOutputDir
+
+	ciop-log "INFO" "Starting fapar processor [/bin/sh /application/CATARSI/batch_Processor/FAPAR_and_LAI/run_fapar.sh /usr/local/MATLAB/MATLAB_Compiler_Runtime/v76/ $myInputDir $myOutputDir]"
+
+	/bin/sh /application/CATARSI/batch_Processor/FAPAR_and_LAI/run_fapar.sh /usr/local/MATLAB/MATLAB_Compiler_Runtime/v76/ $myInputDir $myOutputDir
+
+	cd $outputDir
+	tar cvfz `echo $file | sed 's#\.zip##g' | sed 's#input#output#g'`.tgz `echo $file | sed 's#\.zip##g' | sed 's#input#output#g'`
+	ls -l
+	ciop-log "INFO" "Publishing output [`echo $file | sed 's#\.zip##g' | sed 's#input#output#g'`]"
+	ciop-publish -m $outputDir/`echo $file | sed 's#\.zip##g' | sed 's#input#output#g'`.tgz
+
 done
